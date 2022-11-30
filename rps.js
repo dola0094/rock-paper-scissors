6eef9bb73510b1982c01d9350c68c74b28de6173
Aaron <dola0094@gmail.com>
2022-11-29 21:59:26 -0600

    plan solution before starting

---
+++ rps.js
@@ -0,0 +1,7 @@
+//Start Game
+//Ask player to choose Rock, Paper, or Scissors
+//Computer randomly returns Rock, Paper, or Scissors 
+//Declare a winner of the round, display what beats what
+//Repeat game for 5 rounds 
+//Display winner or loser of 5 rounds
+//Try again button
